d66054fd6c3c9b8fbbc3da91208755f57e70ce84
Shubham Kumar <shubham.kr2951@gmail.com>
2025-03-17 22:38:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="w-screen flex justify-center bg-[#5542ff] py-4 text-blue-50">
+      <p className="text-sm font-light md:text-left">
+        &copy; Zentry 2025 All rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
